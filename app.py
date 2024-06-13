5887a94803d3869bda0f50087e02434110653743
hikitsuri <hikitsuri@hotmail.com>
2024-06-12 18:08:12 -0700

    Bind to $PORT for Heroku compatibility

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask
 import joblib
-import pandas as pd
-from waitress import serve
 
 app = Flask(__name__)
 
-# Load model and scaler
 model = joblib.load('soil_moisture_model.pkl')
-scaler = joblib.load('scaler.pkl')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    new_data = pd.DataFrame(data)
-    
-    # Scale new data
-    new_data_scaled = scaler.transform(new_data)
-    
-    # Predict
-    prediction = model.predict(new_data_scaled)
-    return jsonify({'predicted_soil_moisture': prediction.tolist()})
+@app.route('/')
+def home():
+    return "Hello, this is the irrigation prediction app!"
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
